d637a760ec3dd671e798beba1fe0cc30bf4510c9
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2023-10-26 21:11:35 -0300

    Adicionando menu oculto

---
+++ Script/script.js
@@ -0,0 +1,12 @@
+function abrirNav(){
+    document.getElementById("menuOculto").style.width = "250px";
+    document.getElementById("principal").style.marginLeft = "250px";
+}
+
+function fecharNav(){
+    document.getElementById("menuOculto").style.width = "0";
+    document.getElementById("principal").style.marginLeft = "0";
+}
+
+
+
